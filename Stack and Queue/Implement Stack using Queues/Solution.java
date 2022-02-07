38a35f7d8a139d8eaa818bd7ef113f9ba8da3887
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-02-07 13:40:28 +0530

    Update Solution.java

--- Stack and Queue/Implement Stack using Queues/Solution.java
+++ Stack and Queue/Implement Stack using Queues/Solution.java
@@ -1,4 +1,46 @@
+class MyStack {
+    //1 queue, O(N) INSERT O(1) DELETE
+    int top;
+    Queue<Integer> queue;
+    boolean isEmpty;
+    public MyStack() {
+        top = -1;
+        queue = new LinkedList<Integer>();
+        isEmpty = true;
+    }
+    
+    public void push(int x) {
+        top = x;
+        queue.add(x);
+        int size = queue.size();
+        while(size-- > 1) {
+            queue.add(queue.remove());
+        }
+    }
+    
+    public int pop() {
+        return queue.remove();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        if(queue.isEmpty())
+             return true;
+         return false;
+    }
+}
 
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
 
 // class MyStack {
 //     //1 queue, O(1) INSERT, O(N) DELETE
