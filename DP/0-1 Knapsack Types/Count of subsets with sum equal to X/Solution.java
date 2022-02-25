b92136749f07733d3dc2a206e2b0ae740b514f18
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-02-25 17:24:35 +0530

    Update Solution.java

--- DP/0-1 Knapsack Types/Count of subsets with sum equal to X/Solution.java
+++ DP/0-1 Knapsack Types/Count of subsets with sum equal to X/Solution.java
@@ -1,1 +1,25 @@
-.
+class Solution{
+
+    static Boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        
+        int[][] dp = new int[N+1][sum+1];
+        
+        for(int i = 0; i <= N; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j <= sum; j++) {
+            dp[0][j] = 0;
+        }
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= sum; j++) {
+                if(arr[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][sum];
+    }
+}
