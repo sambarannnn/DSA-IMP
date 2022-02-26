93966bc6d1411129cd8bfafde73bca3119a05eb9
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-02-26 12:31:52 +0530

    Create Solution.java

---
+++ DP/Unbounded KnapSack Types/Coin Change 2 : Count Ways/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        //count of subset sum but usng unbounded knapsack
+        int[][] dp = new int[coins.length+1][amount+1];
+        
+        for(int i = 0; i <= coins.length; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j <= amount; j++) {
+            dp[0][j] = 0;
+        }
+        
+        for(int i = 1; i <= coins.length; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
