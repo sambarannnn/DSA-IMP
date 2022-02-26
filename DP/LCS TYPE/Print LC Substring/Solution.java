e6333aa588b4fe27f825f08e032914f29a425d38
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-02-26 18:55:48 +0530

    Create Solution.java

---
+++ DP/LCS TYPE/Print LC Substring/Solution.java
@@ -0,0 +1,38 @@
+class Solution{
+    int longestCommonSubstr(String S1, String S2, int n, int m){
+        // code here
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+        int longest = 0;
+        int row = 0;
+        int col = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(S1.charAt(i-1) == S2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];//making the substring diagonally
+                    if(dp[i][j] > longest) {
+                        longest = dp[i][j];
+                        row = i;
+                        col = i;
+                    }
+                } else {
+                    dp[i][j] = 0;//reset it to 0
+                }
+            }
+        }
+        StringBuilder substr_rev = new StringBuilder();
+        while (dp[row][col] != 0) {
+            substr_rev.append(S1.charAt(row - 1)) // or Y[col-1]
+            // move diagonally up to previous cell
+            row--;
+            col--;
+        }
+        String substr_result = substr_rev.reverse().toString();
+        return substr_result;
+    }
+}
