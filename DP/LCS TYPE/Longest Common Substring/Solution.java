4cccfe482aa7b1ad7d654edd519788e8abdf4f57
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-02-26 18:55:30 +0530

    Update Solution.java

--- DP/LCS TYPE/Longest Common Substring/Solution.java
+++ DP/LCS TYPE/Longest Common Substring/Solution.java
@@ -12,8 +12,8 @@ class Solution{
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
                 if(S1.charAt(i-1) == S2.charAt(j-1)) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    longest = Math.max(dp[i][j], longest);//making the substring diagonally
+                    dp[i][j] = 1 + dp[i-1][j-1];//Making the substring diagonally
+                    longest = Math.max(dp[i][j], longest);
                 } else {
                     dp[i][j] = 0;//reset it to 0
                 }
