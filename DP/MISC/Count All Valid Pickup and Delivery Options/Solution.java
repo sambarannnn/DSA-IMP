cda74c12172254f582b7e46acd28262a0260b876
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-03-06 23:16:19 +0530

    Create Solution.java

---
+++ DP/MISC/Count All Valid Pickup and Delivery Options/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countOrders(int n) {
+        long mod =(long) 1e9+7;
+        long[] dp = new long[501];
+        dp[1] = 1;
+        dp[2] = 6;
+        
+        for (int i = 3; i <= n; ++i) {
+            int places = 2 * i - 1;
+            int multi = places * (places + 1) / 2;
+            dp[i] = (dp[i-1] * multi) % mod;
+        }
+        
+        return (int) dp[n];
+    }
+}
