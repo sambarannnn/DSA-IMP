7321d3cb2494ac11d07341e15df8ded34487de76
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-03-11 15:47:21 +0530

    Update Solution.java

--- Arrays/Maximum Subarray/Solution.java
+++ Arrays/Maximum Subarray/Solution.java
@@ -1,16 +1,25 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        
-        for(int num : nums) {
-            sum += num;
-            
-            max = Math.max(max, sum);
-            
-            if(sum < 0) sum = 0;//if sum becomes negative, just reset sum counter.
-            //single negative value is taken into consideration for max as we check for max before resetting
+        int[] dp = new int[nums.length];
+        int maxSum = nums[0];
+        dp[0] = nums[0];//current max subarray till now including this index
+        for(int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i-1]);
+            maxSum = Math.max(maxSum, dp[i]);//max of all foud current maxes
         }
-        return max;
+        return maxSum;
+        
+        //KADANE'S ALGO IMP
+        // int sum = 0;
+        // int maxSum = Integer.MIN_VALUE;
+        // for(int num : nums) {
+        //     sum += num;
+        //     maxSum = Math.max(maxSum, sum);
+        //     if(sum < 0) {
+        //         sum = 0;
+        //     }
+        // }
+        // return maxSum;
     }
+    
 }
