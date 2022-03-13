c35ed38b7af5254843b6b8f00ce2c68c21c77585
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-03-13 15:33:27 +0530

    Create Solution.java

---
+++ DP/Kadane's Algo/Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int currMin = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - currMin);
+            currMin = Math.min(currMin, prices[i]);
+        }
+        return maxProfit;
+    }
+}
