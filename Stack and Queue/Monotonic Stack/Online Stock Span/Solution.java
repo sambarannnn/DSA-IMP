d2587468bc2544d063feb2ccdd70befc6ba91195
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-05-07 18:20:47 +0530

    Update Solution.java

--- Stack and Queue/Monotonic Stack/Online Stock Span/Solution.java
+++ Stack and Queue/Monotonic Stack/Online Stock Span/Solution.java
@@ -1,28 +1,30 @@
 class StockSpanner {
-
-    ArrayList<Integer> stocks;
-    Stack<Integer> stack;
+    //use alternate version of nge
+    //to calculate nge on left, we usually traverse from right end of array, this is alt code to traverse from left and still do it
     int index;
+    Stack<Integer> stack;
+    List<Integer> stocks;
     public StockSpanner() {
+        index = 0;
         stack = new Stack<Integer>();
         stocks = new ArrayList<Integer>();
-        index = 0;
     }
     
+    //think of this as individual iterations of a for loop
     public int next(int price) {
-        //keep finding nge on left
         stocks.add(price);
         while(!stack.isEmpty() && stocks.get(stack.peek()) <= price) {
             stack.pop();
         }
-        int res = 1;
-        if(stack.isEmpty()) {
+        //now stack.top should hold next greater element on left for price
+        int res = -1;
+        if(stack.isEmpty()) {//no nge on left
             res = index+1;
         } else {
-            res = index - (stack.peek()+1) + 1;
+            res = index - stack.peek(); 
         }
         stack.push(index);
-        index++;//for upcoming price
+        index++;
         return res;
     }
 }
